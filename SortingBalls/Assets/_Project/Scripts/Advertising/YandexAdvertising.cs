113ad0b88596f6e5abce4996a29903960cbdd57e
Рамис <doof3241@gmail.com>
2026-06-27 04:36:48 +0500

    Save

---
+++ SortingBalls/Assets/_Project/Scripts/Advertising/YandexAdvertising.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Project.Scripts.Advertising
+{
+
+    public class YandexAdvertising : IAdvertising
+    {
+        public void Dispose()
+        {
+            
+        }
+    }
+}
